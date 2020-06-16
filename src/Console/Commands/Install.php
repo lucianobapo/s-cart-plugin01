fd2569c8fc320fcc136c78d4457c149527c21aa8
Luciano Porto <luciano.bapo@gmail.com>
2020-06-16 16:23:34 -0300

    cmt

--- src/Console/Commands/Install.php
+++ src/Console/Commands/Install.php
@@ -52,63 +52,10 @@ class Install extends Command
         $this->progressBar->advance();
 
         $this->line(' Publishing files');
-        $this->callSilent('vendor:publish', ['--tag' => 'erpnetVideoPlugin']);
+        $this->callSilent('vendor:publish', ['--force' => true, '--tag' => 'erpnetVideoPlugin']);
 
         $this->progressBar->finish();
         $this->info($this->title." installation finished.");
     }
 
-    /**
-     * Run a SSH command.
-     *
-     * @param string $command      The SSH command that needs to be run
-     * @param bool   $beforeNotice Information for the user before the command is run
-     * @param bool   $afterNotice  Information for the user after the command is run
-     *
-     * @return mixed Command-line output
-     */
-    public function executeProcess($command, $beforeNotice = false, $afterNotice = false):void
-    {
-        $this->echo('info', $beforeNotice ? ' '.$beforeNotice : $command);
-
-        $process = new Process($command, null, null, null, $this->option('timeout'), null);
-        $process->run(function ($type, $buffer) {
-            if (Process::ERR === $type) {
-                $this->echo('comment', $buffer);
-            } else {
-                $this->echo('line', $buffer);
-            }
-        });
-
-        // executes after the command finishes
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-
-        if ($this->progressBar) {
-            $this->progressBar->advance();
-        }
-
-        if ($afterNotice) {
-            $this->echo('info', $afterNotice);
-        }
-    }
-
-    /**
-     * Write text to the screen for the user to see.
-     *
-     * @param [string] $type    line, info, comment, question, error
-     * @param [string] $content
-     */
-    public function echo($type, $content)
-    {
-        if ($this->option('debug') == false) {
-            return;
-        }
-
-        // skip empty lines
-        if (trim($content)) {
-            $this->{$type}($content);
-        }
-    }
 }
